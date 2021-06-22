1c928e5e87751360a0172658e0e3ddab49dc6737
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-06-21 23:01:05 -0300

    Add express, nodemon, criando routes e fix's e por fim, template engine com ejs

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const server = express()
+
+const routes = require("./routes")
+
+// usando template engine
+server.set('view engine', 'ejs')
+
+// habilitar arquivos staticos
+server.use(express.static("public"))
+
+// routes
+
+server.use(routes)
+server.listen(3000, () => console.log('server rodando...'))
