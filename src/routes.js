898454bfa9828e47d98e4c2512dff7022fc05a89
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-06-29 20:12:04 -0300

    Configuração e atualização de jobs no index

--- src/routes.js
+++ src/routes.js
@@ -12,11 +12,38 @@ const profile = {
     "vacation-per-year": 4
 }
 
-routes.get('/', (req, res) =>  res.render(views + "index"))
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 60,
+        created_at: Date.now() // atribuindo a data de hoje
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at: Date.now() // atribuindo a data de hoje
+    }
+]
+
+routes.get('/', (req, res) =>  res.render(views + "index", { jobs }))
 
 routes.get('/job', (req, res) =>  res.render(views + "job"))
 routes.post('/job', (req, res) =>  {
-    console.log(req.body);
+
+    const lastId = jobs[jobs.length - 1]?.id || 1;
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now() // atribuindo a data de hoje
+    })
+    return res.redirect('/')
 })
 
 routes.get('/job/edit', (req, res) =>  res.render(views + "job-edit"))
