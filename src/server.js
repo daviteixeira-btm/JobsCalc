bbbf589ff5cceafaaf59804ce8ffe22d3ca6b2ae
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-07-10 03:53:08 -0300

    Refatorar routes para uma nova forma de chamar views

--- src/server.js
+++ src/server.js
@@ -4,8 +4,13 @@ const server = express()
 
 const routes = require("./routes")
 
+const path = require("path")
+
 // usando template engine
 server.set('view engine', 'ejs')
+
+// Mudar a localização da pasta views
+server.set('views', path.join(__dirname, 'views'))
 
 // habilitar arquivos staticos
 server.use(express.static("public"))
