d358d344530b722fd96754ffc420f32232a6e32b
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-07-11 23:54:37 -0300

    Iniciando a refatoração dos Jobs

---
+++ src/model/Job.js
@@ -0,0 +1,22 @@
+let data = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 1,
+        created_at: Date.now() // atribuindo a data de hoje
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at: Date.now() // atribuindo a data de hoje
+    }
+];
+
+module.exports = {
+    get(){
+        return data
+    }
+}
