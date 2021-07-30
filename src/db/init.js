51ce469d540565e894c0622366c4dfb9cf89536a
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-07-29 23:55:13 -0300

    Conexão com o  Banco de Dados e profile

--- src/db/init.js
+++ src/db/init.js
@@ -32,14 +32,16 @@ const initDb = {
             monthly_budget, 
             days_per_week, 
             hours_per_day, 
-            vacation_per_year
+            vacation_per_year,
+            value_hour
         ) VALUES (
             "Davi Teixeira",
             "https://avatars.githubusercontent.com/u/35413910?v=4",
             3000,
             5,
             5,
-            4
+            4,
+            70
         );`);
 
         await db.run(`INSERT INTO jobs (
